ebe7596138ac5a5cd73afce260db835b86a7db44
Brian Brooks Jr. <brianbrooksjr1@gmail.com>
2025-10-03 14:17:20 -0500

    Added .split and its delimiting pattern for fullName

--- src/main/java/com/pluralsight/FullNameParser.java
+++ src/main/java/com/pluralsight/FullNameParser.java
@@ -1,3 +1,5 @@
+package com.pluralsight;
+
 import java.util.Scanner;
 
 public class FullNameParser {
@@ -17,23 +19,6 @@ public class FullNameParser {
         String middleName = "(none)";
         String lastName = "(none)";
 
-        // Assign values based on the number of name parts
-        if (nameParts.length >= 1) {
-            firstName = nameParts[0];  // First name is always the first part
-        }
-        if (nameParts.length == 3) {
-            middleName = nameParts[1];  // Middle name is the second part
-            lastName = nameParts[2];     // Last name is the third part
-        } else if (nameParts.length == 2) {
-            lastName = nameParts[1];     // Last name is the second part
-        }
 
-        // Display the results
-        System.out.println("First name: " + firstName);
-        System.out.println("Middle name: " + middleName);
-        System.out.println("Last name: " + lastName);
-
-        // Close the scanner
-        scanner.close();
     }
 }
